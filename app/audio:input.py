1275d829a34002738bf1a070760235eaa65ab638
Phoenix <41755735+johnnyknoxville1337@users.noreply.github.com>
2023-03-26 13:53:02 +0200

    Create audio:input.py

---
+++ app/audio:input.py
@@ -0,0 +1,26 @@
+import gradio as gr
+import openai
+from pathlib import Path
+import os
+
+# openai.api_key = os.environ.get('OPEN_API_KEY')
+openai.api_key = "sk-"
+
+
+def transcribe(audio):
+    print(audio)
+    
+    myfile=Path(audio)
+    myfile=myfile.rename(myfile.with_suffix('.wav'))
+
+    audio_file= open(myfile, "rb")
+ 
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    print(transcript)
+
+    return transcript["text"]
+
+demo = gr.Interface(fn=transcribe, inputs=gr.Audio(source="microphone", type="filepath"), outputs="text")
+
+demo.launch()
+#demo.launch(share=True)
