d5a470a426d8a408eccbfe9ea7c1a30248dd9136
Phoenix <johnnyknoxville1337@outlook.com>
2023-04-02 16:56:31 +0200

    Added WhatsApp API

--- app/vectorizers/sentence_transformer.py
+++ app/vectorizers/sentence_transformer.py
@@ -4,10 +4,11 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 
 model = SentenceTransformer(
-    'sentence-transformers/all-distilroberta-v1', cache_folder='/home/chatbot_fastapi/', device='cuda:0'
+    'sentence-transformers/all-distilroberta-v1', cache_folder='/home/chatbot_fastapi/app/data/'
 )
 
 MAX_TEXT_LENGTH = 400
+NUMBER_PRODUCTS = 10000
 
 
 def auto_truncate(val):
@@ -15,12 +16,13 @@ def auto_truncate(val):
 
 
 def get_data_vectors(room_data: pd.DataFrame):
-    logging.warning(f'Vectorizing index data...')
+    logging.info(f'Vectorizing index data...')
+
     # get the first 1000 products with non-empty item keywords
-    rooms_metadata = room_data.to_dict(orient='index')
+    rooms_metadata = room_data.head(NUMBER_PRODUCTS).to_dict(orient='index')
     item_keywords = [
         ', '.join(auto_truncate(rooms_metadata[i][key]) for key in [
-            'city', 'description', 'price'
+            'city', 'description','price'
             ]
         )
         for i in rooms_metadata.keys()
